9b2fde7bf5952b39a34891c154613e51a0304b73
HHMruhloff <mruhloff@proton.me>
2026-04-17 14:14:21 +0200

    Update art.c

--- boards/shields/nice_view_disp/widgets/art.c
+++ boards/shields/nice_view_disp/widgets/art.c
@@ -8,16 +8,19 @@
 #define LV_ATTRIBUTE_IMG_BALLOON
 #endif
 
-// Balloon bleibt komplett unsichtbar
+/* Balloon Image deaktiviert */
 const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_BALLOON uint8_t
-    balloon_map[] = { 0x00, 0x00, 0x00, 0xff, 0xff, 0xff, 0xff, 0xff };
+    balloon_map[] = {
+        0x00, 0x00, 0x00, 0xff, /* Index 0 */
+        0xff, 0xff, 0xff, 0xff  /* Index 1 */
+};
 
 const lv_img_dsc_t balloon = {
     .header.cf = LV_IMG_CF_INDEXED_1BIT,
     .header.always_zero = 0,
     .header.reserved = 0,
-    .header.w = 0,
-    .header.h = 0,
+    .header.w = 0, // Breite auf 0 gesetzt
+    .header.h = 0, // Höhe auf 0 gesetzt
     .data_size = 8,
     .data = balloon_map,
 };
@@ -26,27 +29,19 @@ const lv_img_dsc_t balloon = {
 #define LV_ATTRIBUTE_IMG_MOUNTAIN
 #endif
 
-// Hier ist das "M" als Bitmap (fett gezeichnet)
+/* Mountain Image deaktiviert */
 const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_MOUNTAIN uint8_t
     mountain_map[] = {
-#if CONFIG_NICE_VIEW_DISP_WIDGET_INVERTED
-        0xff, 0xff, 0xff, 0xff, 0x00, 0x00, 0x00, 0xff,
-#else
-        0x00, 0x00, 0x00, 0xff, 0xff, 0xff, 0xff, 0xff,
-#endif
-        // Pixel-Daten für ein fettes "M" (20x20 Pixel)
-        0xFF, 0xFF, 0xF0, 0xC1, 0x81, 0xF0, 0xC1, 0x81, 0xF0, 0xD3, 0x91, 0xF0, 
-        0xD3, 0x91, 0xF0, 0xDB, 0xB1, 0xF0, 0xDB, 0xB1, 0xF0, 0xDB, 0xB1, 0xF0,
-        0xC1, 0x81, 0xF0, 0xC1, 0x81, 0xF0, 0xC1, 0x81, 0xF0, 0xC1, 0x81, 0xF0,
-        0xC1, 0x81, 0xF0, 0xC1, 0x81, 0xF0, 0xC1, 0x81, 0xF0, 0xFF, 0xFF, 0xF0
-    };
+        0x00, 0x00, 0x00, 0xff, /* Index 0 */
+        0xff, 0xff, 0xff, 0xff  /* Index 1 */
+};
 
 const lv_img_dsc_t mountain = {
     .header.cf = LV_IMG_CF_INDEXED_1BIT,
     .header.always_zero = 0,
     .header.reserved = 0,
-    .header.w = 20, // Breite des M
-    .header.h = 16, // Höhe des M
-    .data_size = sizeof(mountain_map),
+    .header.w = 0, // Breite auf 0 gesetzt
+    .header.h = 0, // Höhe auf 0 gesetzt
+    .data_size = 8,
     .data = mountain_map,
 };
